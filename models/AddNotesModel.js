953e5b3858c207e2bb759dfb89e1cca6069b12a6
muhammed-rasiq <razicodex@gmail.com>
2026-08-31 22:56:27 +0530

    wrote shema code

---
+++ models/AddNotesModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const addNotesSchema = new mongoose.Schema({
+    NoteTitle:{
+        type:String,
+        required:true
+    },
+    Category:{
+        type:String,
+        required:true
+    },
+    Note:{
+        type:String,
+        required:true
+    },
+    Tags:{
+        type:String,
+        required:true
+    }
+})
+
+module.exports = mongoose.model('UsersNotes',addNotesSchema)
