0a959902fdde68261e9f15d1557a4657ff033ed3
muhammed-rasiq <razicodex@gmail.com>
2026-09-05 22:18:45 +0530

    installed corse and implemented

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const dbConnect = require('./config/db')
 const router = require('./router/route')
+const corse = require('cors')
 
 
 const app = express()
+
+app.use(corse())
 
 app.use(express.json())
 app.use(router)
 
 dbConnect()
+
+
 
 const port = 3000
 app.listen(port,()=>{
